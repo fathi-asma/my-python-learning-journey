a4259460370eb36bc832a2ff1d4e2258cc6164a7
fathi-Asma <fathuasma456@gmail.com>
2025-06-28 22:08:20 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,66 @@
+# Addition integer value from user
+num1 = input ("Enter the first number: ")
+num2 = input ("Enter the second number: ")
+sum = int(num1) + int(num2)
+print ("Addition (int):", sum)
+
+# Addition for decimal numbers
+num1 = input ("Enter the number 1: ")
+num2 = input ("Enter the second number: ")
+sum = float(num1) + float(num2)
+print ("Addition (float): ", sum)
+
+
+# Substraction for two integer values
+num1 = input ("Enter the first number: ")
+num2 = input ("Enter the second number: ")
+sum = int(num1) - int(num2)
+print ("substraction (int): ", sum)
+
+
+# Substraction for two decimal values
+num1 = input ("Enter the first number: ")
+num2 = input ("Enter the second number: ")
+sum = float(num1) - float(num2)
+print ("Sustraction (float): " , sum)
+
+# Multiplication for two integer values
+num1 = input ("Enter the first number: ")
+num2 = input ("Enter the second number: ")
+total = int(num1) * int(num2)
+print ("Multiplication (int): " , total)
+
+
+# Multiplication for two decimal values
+num1 = input ("Enter the first number: ")
+num2 = input ("Enter the second number: ")
+total = float(num1) * float(num2)
+print ("Multiplication (int): ", total)
+
+
+# Division for two integer values
+num1 = input ("Enter the first number: ")
+num2 = input ("Enter the second number: ")
+result = int(num1) / int(num2)
+print ("Division (int): ", result)
+
+# Division for two decimal values
+num1 = input ("Enter the first number: ")
+num2 = input ("Enter the second number: ")
+result = float(num1) / float(num2)
+print ("Division (float): ", result)
+
+# To tell total is even or odd number
+if (sum % 2 == 0) :
+    print ("even")
+else :
+    print ("odd")
+
+
+
+    
+
+
+
+
+
