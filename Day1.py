0d7f6a8159a32bfbf21288064614c2f5f55c91a1
fathi-Asma <fathuasma456@gmail.com>
2025-06-28 22:07:58 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,6 @@
+# The programe prints the welcom message.
+print ("Welcome to Python Programming!")
+
+#This progarme ask your name and print welcome message.
+name = input ("Enter your name: ")
+print ( name + " "  +  "Welcome you for python programming!")
