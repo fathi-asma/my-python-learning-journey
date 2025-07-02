a0fd8e134b49e107ba29ebd8d3c0e9e2d00b1402
Fathima Asma <fathuasma456@gmail.com>
2025-07-02 20:50:15 +0530

    Add files via upload

--- Day6.py
+++ Day6.py
@@ -5,54 +5,46 @@ greet()
 
 # write a function that takes user name as input and print greeting 
 def name():
-      user = input("Enter your name: ")
       print("Hello!", user)
 name() 
+user = input("Enter your name: ")
 
 # create a function that takes two numbers and prints their sum
-def num():
-     num1 = input("Enter the first number: ")
-     num2 = input("Enter the second number: ")
+def num(num1,num2):
      sum = (int(num1)+ int(num2))
      print("sumation is: ", sum)
-num()
+num(input("Enter number 1: "), (input("Enter number two: ")))
+
 
 # Create a function that takes a number and returns its square.
-def squ():
-     num = input("Enter the number: ")
+def squ(num):
      result = int(num) * int(num)
      print("Square is:" , result)
-squ()   
+squ(input("Enter the number: "))   
 
 # Write a function to check if a number is even or odd.
-def check():
-     num = input("Enter the number: ")
+def check(num):
      if int(num) % 2 == 0 :
           print("even")
      else :
           print("odd")
-check()
+check(input("Enter the number: "))
 
 # Write a function that takes a list of numbers and returns the total sum.
-def numbers():
-     num = input("Enter numbers: ")
+def print(num):
      num_list = [int(x) for x in num.split()]
      total = sum(num_list)
      print(total)
-numbers()  
+print(input("Enter numbers: "))  
 
 # Create a function that returns the largest number from three inputs.
-def largest():
-     num1 = input("Enter the first number: ")
-     num2 = input("Enter the second number: ")
-     num3 = input("Enter the third number: ")
+def largest(num1,num2,num3):
      largest_number = max(int(num1),int(num2),int(num3))
      print("largest number is: ", largest_number)
-largest()  
+largest(input("Enter the first number: "), input("Enter the second number: "),input("Enter the third number: "))  
 
 # Write a function that takes a string and returns the number of vowels in it
-def vowels_find():
-     word = input("Enter the word: ")
+def vowels_find(word):
      vowels = "aeiouAEIOU"
      count = 0
      for char in word:
@@ -60,8 +52,18 @@ def vowels_find():
                count += 1
                return count
             
-result = vowels_find()    
-print ("Number of vowels: " , result)  
+# three line mode
+result1 = input("Enter the word: ")
+find = vowels_find(result1)       
+print("Number of vowels: ", find)
+
+# two line mode
+result2 = input("Enter the word: ")
+print("Number of vowels: ", vowels_find(result2))
+
+# one line mode
+result3 = input("Enter the word: ", vowels_find(input("Enter the word: ")))
+
 
 # Write a function that prints numbers from 1 to 10 using a loop.
 def number():
