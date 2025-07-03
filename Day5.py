1b2b2dd38c0e630423d6063e7b376131bd3a26e8
Fathima Asma <fathuasma456@gmail.com>
2025-07-03 09:07:26 +0530

    Add files via upload

--- Day5.py
+++ Day5.py
@@ -1,55 +1,55 @@
 # create tuple with number from 1 to 10
-number = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-print(number)
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(numbers)
 
 # print the first and last numbers
-print(number[0])
-print(number[9])
+print(numbers[0])
+print(numbers[9])
 
 # check if the number 5 is in the tuple
-if 5 in number:
+if 5 in numbers:
     print("yes in the tuple") 
 else:
     print("not in the tuple")
 
 # find the sum of the all numbers
-total = sum(number)
+total = sum(numbers)
 print(total)
 
 # find the max and min numbers in the tuple
-maximum = max(number)
+maximum = max(numbers)
 print(maximum)
-minimum = min(number)
+minimum = min(numbers)
 print(minimum)
 
 # create the tuple with even number
-for number in number:
+for number in numbers:
     if number % 2 == 0:
         print(number)
 
 # count how many times the number 2 appears in the tuple
-num = (1, 2, 2, 3, 4, 2, 5)
+nums = (1, 2, 2, 3, 4, 2, 5)
 count = 0 
-for num in num:
+for num in nums:
     if num == 2:
         count += 1
         print(count)
 
 # reverse the tuple using slicing
 num = (1, 2, 3, 4, 5)
-reversed_tuple = num[::-1]
+reversed_tuple = nums[::-1]
 print(reversed_tuple)
 
 # create two tuples and joing them together 
 num1 = (1, 2, 3, 4, 5)
 num2 = (6, 7, 8, 9, 10)
-add = num1 + num2
-print(add)
+total = num1 + num2
+print(total)
 
 # use a loop to print only the numbers greater than 5 in a tuple
-num3 = (5, 10, 15, 20, 40)
+numbers = (5, 10, 15, 20, 40)
 found = False
-for num in num3:
+for num in numbers:
     if num > 5:
         found = True
         print(num)
